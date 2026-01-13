7dffee27e661917384c35a43daeae91ead5841f5
Yogesh GR <yogeshgr@Yogeshs-MacBook-Air.local>
2026-01-14 01:10:43 +0530

    Fix API base URL config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL,
-  localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
-  proxyUrl: import.meta.env.VITE_APP_PROXYURL,
+  baseurl: import.meta.env.VITE_API_BASE_URL,
 };
 
 export default config;
